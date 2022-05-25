af9d3974ba144ed8aac8c98d6be20443f13826bb
tayeful912 <kazitayefulislam12@gmail.com>
2022-05-25 16:55:17 +0600

    Manage User complete with full functionality

---
+++ src/Pages/Dashboard/ManageOders/ManageOders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ManageOders = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ManageOders;
--- src/Pages/Dashboard/ManageUser/ManageUser.js
+++ src/Pages/Dashboard/ManageUser/ManageUser.js
@@ -1,12 +1,44 @@
+import axios from 'axios';
 import React from 'react';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 
 const ManageUser = () => {
 
     const url = `http://localhost:5000/users`
 
-    const { data } = useQuery('manageUser', () => fetch(url)
+    const { data, refetch } = useQuery('manageUser', () => fetch(url)
         .then(res => res.json()))
+
+
+    const handleAdmin = (email) => {
+        console.log(email)
+        const url = `http://localhost:5000/makeAdmin/${email}`;
+        axios.post(url)
+            .then(function (response) {
+                console.log(response)
+                if (response?.data?.modifiedCount) {
+
+                    toast.success('Admin Successfuly')
+                    refetch()
+                }
+            })
+
+    }
+    const handleUser = (email) => {
+        console.log(email)
+        const url = `http://localhost:5000/makeUser/${email}`;
+        axios.post(url)
+            .then(function (response) {
+                console.log(response)
+                if (response?.data?.modifiedCount) {
+
+                    toast.success('User Successfuly')
+                    refetch()
+                }
+            })
+
+    }
 
     console.log(data)
     return (
@@ -17,7 +49,7 @@ const ManageUser = () => {
                     <tr>
                         <th>#</th>
                         <th>Email</th>
-                        <th>Role</th>
+
                         <th>Action</th>
                     </tr>
                 </thead>
@@ -29,8 +61,23 @@ const ManageUser = () => {
                         data?.map((user, index) => <tr class="hover">
                             <th>{index + 1}</th>
                             <td>{user?.userEmail}</td>
-                            <td><button className='btn'>Make Admin</button></td>
-                            <td><button>Make Admin</button></td>
+
+                            <td>
+
+                                {
+                                    user?.role === 'admin' ?
+                                        <button onClick={() => handleUser(user?.userEmail)} className='btn'>Make User</button>
+                                        :
+                                        <button onClick={() => handleAdmin(user?.userEmail)} className='btn'>Make Admin</button>
+
+
+                                }
+
+
+                            </td>
+
+
+
                         </tr>)
                     }
 
